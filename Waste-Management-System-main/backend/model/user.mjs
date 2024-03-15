0e4423008e8ebfb9c745885c1fd87bd0710796a2
Saptarshi Pani <pani.saptarshi@gmail.com>
2024-03-16 00:19:45 +0530

    final

---
+++ Waste-Management-System-main/backend/model/user.mjs
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },// match parameter to accept email ids in proper format from given regex, which will be checked by node assert
+    password:{
+        type: String,
+        required : true
+    },
+    userID:{
+        type: String
+    },
+    recycled:{
+        type: Number,
+        default:0
+    },
+    compostable:{
+        type: Number,
+        default:0
+    },
+    hazardous:{
+        type: Number,
+        default:0
+    }
+})
+const garbageSchema = new mongoose.Schema({
+    paper:{
+        type: Boolean,
+        default: false,
+    },
+    plastic:{
+        type: Boolean,
+        default: false,
+    },
+    metals:{
+        type: Boolean,
+        default: false,
+    },
+    ewaste:{
+        type: Boolean,
+        default: false,
+    },
+    others:{
+        type: Boolean,
+        default: false,
+    },
+    location:{
+        type: String,
+        required: true
+     },
+    //  expireAt:{
+    //      type: Date,
+    //      expires: 10
+    //  },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'user'
+     }
+})
+const Garbage = mongoose.model('garbage', garbageSchema)
+const User = mongoose.model('user', userSchema)
+
+export {Garbage, User}
